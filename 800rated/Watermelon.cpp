0d61a39aad5ce657066e6cff9360a1cb3ca16763
debarshimaity2004 <debarshimaity14@gmail.com>
2025-09-20 09:45:45 +0530

    Create Watermelon.cpp

---
+++ 800rated/Watermelon.cpp
@@ -0,0 +1,17 @@
+//4A. Watermelon
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+    if (w % 2 == 0 && w > 2)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
+
+// Time Complexity (TC): O(1)
+// Space Complexity (SC): O(1)
