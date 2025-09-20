c3d7ca3e24015e05d5b056e512526afea098fdee
debarshimaity2004 <debarshimaity14@gmail.com>
2025-09-20 09:53:24 +0530

    Create 71A.cpp

---
+++ 800rated/71A.cpp
@@ -0,0 +1,18 @@
+//71A. Way Too Long Words
+#include <bits/stdc++.h>
+using namespace std;    
+
+int main()
+{
+    int words;
+    cin>>words;
+    while(words--)
+    {
+        string s;
+        cin>>s;
+        if(s.length()>10)
+            cout<<s[0]<<s.length()-2<<s[s.length()-1]<<endl;
+        else
+            cout<<s<<endl;
+    }
+}
