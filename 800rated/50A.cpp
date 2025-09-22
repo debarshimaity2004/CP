898e63c6ffe57595178ae3b4dc7ec7b9effb037b
debarshimaity2004 <debarshimaity14@gmail.com>
2025-09-22 16:42:32 +0530

    Create 50A.cpp

---
+++ 800rated/50A.cpp
@@ -0,0 +1,13 @@
+//50A. Domino piling
+#include <bits/stdc++.h>    
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<(n*m)/2<<endl;
+    return 0;
+}
+
+//time complexity: O(1)
+//space complexity: O(1)
