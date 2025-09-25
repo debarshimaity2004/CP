98bac87bdbbca2a100265b96dff48e91160bc351
debarshimaity2004 <debarshimaity14@gmail.com>
2025-09-25 16:11:55 +0530

    Create 1899A.cpp

---
+++ 800rated/1899A.cpp
@@ -0,0 +1,20 @@
+//1899A.Game with Integers
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        if(n%3==0){
+            cout<<"Second"<<endl;
+        }
+        else{
+            cout<<"First"<<endl;
+        }
+    }
+    return 0;
+}
