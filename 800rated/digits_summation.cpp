929d0b9f63ecb644764bccb6d4af9cda4ec0aa9c
Debarshi Maity <debarshimaity14@gmail.com>
2025-11-17 19:39:27 +0530

    digits_summation.cpp

---
+++ 800rated/digits_summation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  long long n,m;
+  int rem1,rem2,sum;
+  cin>>n>>m;
+  rem1=n%10;
+  rem2=m%10;
+  sum=rem1+rem2;
+  cout<<sum<<endl;
+}
