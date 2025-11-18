428a8c87ed26d2a054cf0a6f25360f7ce3c32df5
Debarshi Maity <debarshimaity14@gmail.com>
2025-11-18 20:43:27 +0530

    Add brothers comparison program

---
+++ 800rated/the_brothers.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string fpfn,fpln,spfn,spln;
+    cin>>fpfn>>fpln;
+    cin>>spfn>>spln;
+    if(fpln==spln)
+    cout<<"ARE Brothers"<<endl;
+    else
+    cout<<"NOT";
+}
