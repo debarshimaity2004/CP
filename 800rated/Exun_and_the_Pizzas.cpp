a597d6332b021281d4de6e27271f14dea6e499e0
Debarshi Maity <debarshimaity14@gmail.com>
2025-11-19 21:00:27 +0530

    Add Exun_and_the_Pizzas.cpp with main logic

---
+++ 800rated/Exun_and_the_Pizzas.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,k,r;
+    cin>>n>>k>>r;
+    cout<< (n-k)*r<<endl;
+}
