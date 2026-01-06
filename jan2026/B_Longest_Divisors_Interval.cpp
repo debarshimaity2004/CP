7c26745cff48efddeaf1397b58bce7559b76958a
Debarshi Maity <debarshimaity14@gmail.com>
2026-01-06 17:38:11 +0530

    solving

---
+++ jan2026/B_Longest_Divisors_Interval.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long n;
+    cin>>n;
+    int i=1;
+    while(n%i ==0)
+    i++;
+    cout<<i-1<<'\n';
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
