9818e165380c7704bde45a093b1595cfc5ee8e9b
Debarshi Maity <debarshimaity14@gmail.com>
2026-01-12 18:13:27 +0530

    module 2 practice

---
+++ jan2026/print_pattern_2.cpp
@@ -0,0 +1,36 @@
+// Given an integer n(1<=n<=10), print n rows of the given pattern in minimum length.
+
+// 1
+// 2 3
+// 6 5 4
+// 7 8 9 10
+
+// Sample Input
+// 3
+// Sample Output
+// 1
+// 2 3
+// 6 5 4
+
+#include <bits/stdc++.h>>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int count =1;
+	for(int i=0; i< n;i++){
+		int a[i+1];
+		for(int j=0; j <i+1 ;j++){
+			a[j] = count++;
+		}
+
+		if(i%2 ==0){
+			reverse(a, a+(i+1));
+		}
+		for(int j=0; j <i+1 ;j++){
+			cout<< a[j]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
