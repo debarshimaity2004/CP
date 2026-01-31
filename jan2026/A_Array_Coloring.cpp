97b689e2c2692c1da105040c31306b954b74caa2
Debarshi Maity <debarshimaity14@gmail.com>
2026-01-31 15:38:27 +0530

    CP 800 rated solving

--- jan2026/A_Array_Coloring.cpp
+++ jan2026/A_Array_Coloring.cpp
@@ -1,39 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve()
-{
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    bool ok = true;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        if ((a[i] % 2) == (a[i + 1] % 2))
-        {
-            ok = false;
-            break;
-        }
-    }
-
-    if (ok)
-        cout << "YES\n";
-    else
-        cout << "NO\n";
-}
-
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        solve();
+        long long n;
+        cin >> n;
+        long long a[n];
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+
+        long long count_of_odd = 0; 
+        for (int i = 0; i < n; i++) 
+        {
+            if (a[i] % 2 == 1) 
+                count_of_odd++;
+        }
+        
+        if (count_of_odd % 2 == 1)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
     }
     return 0;
 }
