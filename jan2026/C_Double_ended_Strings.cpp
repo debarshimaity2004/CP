97bf88d1bba72d8b485d14903b41582dbdab1dda
Debarshi Maity <debarshimaity14@gmail.com>
2026-01-31 13:43:59 +0530

    CP 800 rated solving

---
+++ jan2026/C_Double_ended_Strings.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string A, B;
+        cin >> A >> B;
+        int n = A.size(), m = B.size();
+        int ans = 0;
+        for (int len = 1; len <= min(n, m); len++) // min(n,m)
+        {
+            for (int i = 0; i + len <= n; i++) // n
+            {
+                for (int j = 0; j + len <= m; j++) // m
+                {
+                    if (A.substr(i, len) == B.substr(j, len)) // len
+                    {
+                        ans = max(ans, len);
+                    }
+                }
+            }
+        }
+        cout << A.size() + B.size() - 2 * ans << endl;
+    }
+    return 0;
+}
+//done without dp as contraints small the
+
